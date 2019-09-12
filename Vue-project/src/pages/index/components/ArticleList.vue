e591cb1f5dc5cc116c165bb50e54a9bfe8fc784e
Lee Deea <1156407573@qq.com>
2019-09-12 12:00:27 +0800

    url

--- Vue-project/src/pages/index/components/ArticleList.vue
+++ Vue-project/src/pages/index/components/ArticleList.vue
@@ -99,7 +99,7 @@ export default {
       dataAnswer: [{
         'id': 12,
         'answer': '这是一个简单的网站，会存放一些我整理的有关习题，不过个人时间精力有限,题目整理不当的地方或更新进度缓慢请给予谅解并及时反馈给我,十分感谢,联系方式：VX:VsevenV73999',
-        'explanation': '绿色代表题目',
+        'explanation': '绿色代表标题描述信息',
         'ImgExplanation': '黄色代表图片描述或细节分析'
       }],
       dataImge: [{
